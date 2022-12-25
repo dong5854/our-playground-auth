2a3e0dd7e4401cf4fae1fbb59d772e6a7c5b5d92
dong young <dong5854@gmail.com>
2022-12-26 00:36:07 +0900

    feat: add user repository

---
+++ internal/auth/repository/user_repository.go
@@ -0,0 +1,8 @@
+package repository
+
+import "github.com/Team-OurPlayground/our-playground-auth/internal/model"
+
+type UserRepository interface {
+	CreateUser(user *model.User) error
+	FindUserInfoByID(id int) (*model.User, error)
+}
