10a95a143d2788e0c05870344b996c13998f164e
dong young <dong5854@gmail.com>
2022-12-26 01:02:13 +0900

    [login-1] add auth controller

--- internal/auth/controller/auth_controller.go
+++ internal/auth/controller/auth_controller.go
@@ -4,15 +4,35 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"github.com/Team-OurPlayground/our-playground-auth/internal/auth/controller/dto"
+	"github.com/Team-OurPlayground/our-playground-auth/internal/auth/service"
 )
 
 type Auth struct {
+	authService service.AuthService
 }
 
-func NewAuthController() *Auth {
-	return &Auth{}
+func NewAuthController(authService service.AuthService) *Auth {
+	return &Auth{
+		authService: authService,
+	}
 }
 
-func (a *Auth) HealthCheck(c echo.Context) (err error) {
-	return c.String(http.StatusOK, "Hello, Auth!")
+func (a *Auth) SignUp(c echo.Context) (err error) {
+	req := new(dto.SignUpRequest)
+	if err = c.Bind(req); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	err = a.authService.SignUp(req)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+
+	return c.String(http.StatusOK, "OK")
+}
+
+func (a *Auth) SignIn(c echo.Context) (err error) {
+	return nil
 }
