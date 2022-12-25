2ef9f0e29ab91f6f5ba30cbd85e27b911b2a37a2
dong young <dong5854@gmail.com>
2022-12-26 00:53:49 +0900

    [login-1] feat: add auth service interface

---
+++ internal/auth/service/auth_interface.go
@@ -0,0 +1,8 @@
+package service
+
+import "github.com/Team-OurPlayground/our-playground-auth/internal/auth/controller/dto"
+
+type AuthService interface {
+	SignUp(request *dto.SignUpRequest) error
+	SignIn(request *dto.SignInRequest) error
+}
