84c178998a6aacb4c8859756fa4b1c8fa75d7283
dong young <dong5854@gmail.com>
2023-01-06 19:37:22 +0900

    [login-1] fix: edit TokenPairRepository UpdateTokenPair parameter

--- internal/auth/repository/token_pair_repository.go
+++ internal/auth/repository/token_pair_repository.go
@@ -3,7 +3,7 @@ package repository
 import "github.com/Team-OurPlayground/our-playground-auth/internal/model"
 
 type TokenPairRepository interface {
-	CreateTokenPair(tokePair *model.TokenPair) error            // TODO: 토큰이 생성되면 email, AccessToken, RefreshToken 저장, Refresh 토큰 수명과 일치시키기
-	GetTokenPairByEmail(email string) (*model.TokenPair, error) // TODO: RefreshToken 으로 AccessToken 을 새로 발급 받을 때, 두개의 토큰 값의 일치를 찾기 위함
-	UpdateTokenPair(email string) error                         // TODO: Access 토큰을 새로 발급 받은 후 업데이트, Refresh 토큰 수명과 일치시키기
+	CreateTokenPair(tokePair *model.TokenPair) error
+	GetTokenPairByEmail(email string) (*model.TokenPair, error)
+	UpdateTokenPair(tokenPair *model.TokenPair) error
 }
