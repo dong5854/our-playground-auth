fe9fa21285a0a477d5a2ed6a70701e284748a330
dong young <dong5854@gmail.com>
2023-01-06 19:40:15 +0900

    [login-1] feat: implement TokenPair repository redis

---
+++ internal/auth/repository/redis/token_pair.go
@@ -0,0 +1,54 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+
+	"github.com/Team-OurPlayground/our-playground-auth/internal/auth/repository"
+	"github.com/Team-OurPlayground/our-playground-auth/internal/model"
+	"github.com/Team-OurPlayground/our-playground-auth/internal/util/customerror"
+)
+
+type tokenPairRepository struct {
+	redisClient *redis.Client
+}
+
+func NewTokenPairRepository(client *redis.Client) repository.TokenPairRepository {
+	return &tokenPairRepository{
+		redisClient: client,
+	}
+}
+
+func (t *tokenPairRepository) CreateTokenPair(tokenPair *model.TokenPair) error {
+	expiration := tokenPair.ExpiresAt.Sub(time.Now())
+	err := t.redisClient.Set(context.TODO(), tokenPair.Email, tokenPair, expiration).Err()
+	if err != nil {
+		return customerror.Wrap(err, customerror.ErrDBInternal, "CreateTokenPair error")
+	}
+	return nil
+}
+
+func (t *tokenPairRepository) GetTokenPairByEmail(email string) (*model.TokenPair, error) {
+	resultByte, err := t.redisClient.Get(context.TODO(), email).Bytes()
+	if err != nil {
+		return nil, customerror.Wrap(err, customerror.ErrDBInternal, "redis get error: GetTokenPairByEmail")
+	}
+	result := new(model.TokenPair)
+	err = json.Unmarshal(resultByte, result)
+	if err != nil {
+		return nil, customerror.Wrap(err, customerror.ErrInternalServer, "json.Unmarshal error: GetTokenPairByEmail")
+	}
+	return result, nil
+}
+
+func (t *tokenPairRepository) UpdateTokenPair(tokenPair *model.TokenPair) error {
+	expiration := tokenPair.ExpiresAt.Sub(time.Now())
+	err := t.redisClient.Set(context.TODO(), tokenPair.Email, tokenPair, expiration).Err()
+	if err != nil {
+		return customerror.Wrap(err, customerror.ErrDBInternal, "UpdateTokenPair error")
+	}
+	return nil
+}
